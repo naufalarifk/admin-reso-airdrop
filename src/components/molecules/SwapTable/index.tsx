af5d497ec06b2ad25047b0b3ed480ed293593a6c
naufalarifk <abnormalchild414@gmail.com>
2024-04-18 11:34:19 +0700

    chore: fix pool and swap

--- src/components/molecules/SwapTable/index.tsx
+++ src/components/molecules/SwapTable/index.tsx
@@ -1,10 +1,11 @@
 import { Text } from "@/components"
+import { cn } from "@/utils"
 import { ReactNode } from "react"
 
-export const SwapTable = ({ row, col }: { row: string[], col: ReactNode[] }) => {
+export const SwapTable = ({ row, col, className }: { row: string[], col: ReactNode[], className?: string }) => {
     return (
         <>
-            <div className={`grid grid-cols-${row.length} my-4 border-b text-center`}>
+            <div className={cn(`grid grid-cols-${row.length} my-4 border-b text-center`, className)}>
                 {row.map((item) => <Text>{item}</Text>)}
             </div>
             {col}
