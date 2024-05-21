11653dd49ad11b2bc8b6244ca329dfa4d173e1b8
azharluthfi14 <azharluthfi4@gmail.com>
2024-05-21 08:21:01 +0700

    chore: add price animation

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -234,48 +234,7 @@ export const Swap = () => {
     },
   ];
 
-  const token_rate = [
-    {
-      name: "BTC/USDT",
-      rate: "+0.25%",
-    },
-    {
-      name: "ETH/USDT",
-      rate: "+0.25%",
-    },
-    {
-      name: "XRP/USDT",
-      rate: "+0.25%",
-    },
-    {
-      name: "XRP/ETH",
-      rate: "+0.25%",
-    },
-    {
-      name: "ETH/BTC",
-      rate: "+0.25%",
-    },
-    {
-      name: "DOGE/USDT",
-      rate: "+0.25%",
-    },
-    {
-      name: "XRP/DOGE",
-      rate: "+0.25%",
-    },
-    {
-      name: "ARB/ETH",
-      rate: "+0.25%",
-    },
-    {
-      name: "ARB/ETH",
-      rate: "+0.25%",
-    },
-    {
-      name: "ARB/ETH",
-      rate: "+0.25%",
-    },
-  ];
+
 
   const my_orders = [
     {
