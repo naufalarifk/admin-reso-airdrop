0c0cbd6a8c4914bde9d9bf65009e324a5a9ee9a8
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2024-05-22 10:47:06 +0700

    ws connect order book

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,58 +1,88 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import {
-  CreateStakingPage,
-  // Landing,
-  Staking,
-  // Trade,
-  Bridge,
-  Swap,
-  Terms,
-  Privacy,
-  Pool,
-  NotFound,
-  DummySwap,
-} from "@/pages";
-import { MainMenu } from "./MainMenu";
-import { LayoutDashboard, StakeLayout } from "@/components";
-import { useScrollTop } from "@/hooks";
-import { Dummy } from "@/pages/Dummy";
+   CreateStakingPage,
+   // Landing,
+   Staking,
+   // Trade,
+   Bridge,
+   Swap,
+   Terms,
+   Privacy,
+   Pool,
+   NotFound,
+   DummySwap,
+} from '@/pages';
+import { MainMenu } from './MainMenu';
+import { LayoutDashboard, StakeLayout } from '@/components';
+import { useScrollTop } from '@/hooks';
+import { Dummy } from '@/pages/Dummy';
 
 export const RootLayout = () => {
-  useScrollTop();
-  // const { chain } = useAccount();
+   useScrollTop();
+   // const { chain } = useAccount();
 
-  // useEffect(() => {
-  //   const url = new URL(window.location.h66ref);
-  //   url.searchParams.set(
-  //     "chainId",
-  //     String(chain?.nativeCurrency?.symbol).toLowerCase()
-  //   );
-  //   if (url.href !== window.location.href && chain?.id !== undefined) {
-  //     window.location.href = url.href;
-  //   }
-  // });
+   // useEffect(() => {
+   //   const url = new URL(window.location.h66ref);
+   //   url.searchParams.set(
+   //     "chainId",
+   //     String(chain?.nativeCurrency?.symbol).toLowerCase()
+   //   );
+   //   if (url.href !== window.location.href && chain?.id !== undefined) {
+   //     window.location.href = url.href;
+   //   }
+   // });
 
-  return (
-    <Routes>
-      {/* <Route path="/" element={<Landing />} /> */}
-      {/* <Route path="/" element={<Navigate to="/swap/btcusd" replace />} /> */}
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route element={<MainMenu />}>
-        <Route path="/*" element={<NotFound />} />
-        <Route element={<LayoutDashboard />}>
-          {/* <Route path="/trade" element={<Trade />} /> */}
-          <Route path="/bridge" element={<Bridge />} />
-          <Route path="/swap/:market" element={<Swap />} />
-          <Route path="/pool" element={<Pool />} />
-          <Route path="/dummy/:market" element={<Dummy />} />
-          <Route path="/dummyswap/:market" element={<DummySwap />} />
-        </Route>
-      </Route>
-      <Route element={<StakeLayout />}>
-        <Route path="/staking" element={<Staking />} />
-        <Route path="/staking/create" element={<CreateStakingPage />} />
-      </Route>
-    </Routes>
-  );
+   return (
+      <Routes>
+         {/* <Route path="/" element={<Landing />} /> */}
+         {/* <Route path="/" element={<Navigate to="/swap/btcusd" replace />} /> */}
+         <Route
+            path="/terms"
+            element={<Terms />}
+         />
+         <Route
+            path="/privacy"
+            element={<Privacy />}
+         />
+         <Route element={<MainMenu />}>
+            <Route
+               path="/*"
+               element={<NotFound />}
+            />
+            <Route element={<LayoutDashboard />}>
+               {/* <Route path="/trade" element={<Trade />} /> */}
+               <Route
+                  path="/bridge"
+                  element={<Bridge />}
+               />
+               <Route
+                  path="/swap/:market"
+                  element={<DummySwap />}
+               />
+               <Route
+                  path="/pool"
+                  element={<Pool />}
+               />
+               <Route
+                  path="/dummy/:market"
+                  element={<Dummy />}
+               />
+               <Route
+                  path="/dummyswap/:market"
+                  element={<Swap />}
+               />
+            </Route>
+         </Route>
+         <Route element={<StakeLayout />}>
+            <Route
+               path="/staking"
+               element={<Staking />}
+            />
+            <Route
+               path="/staking/create"
+               element={<CreateStakingPage />}
+            />
+         </Route>
+      </Routes>
+   );
 };
