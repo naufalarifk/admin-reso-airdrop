43cf8ff0c5baa6bf722fa8de57a3e7de69cf548a
naufalarifk <abnormalchild414@gmail.com>
2024-05-25 17:38:29 +0700

    chore: fix create new market

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -157,7 +157,7 @@ export const Header = ({ isLanding = false }: { isLanding?: boolean }) => {
                       ) : item.code === 'createnewmarket' ? (
                         <div
                           onClick={() => navigate('/market')}
-                          className="flex cursor-pointer items-center gap-3 rounded-full bg-primary/10 px-4 py-2 text-primary">
+                          className="flex font-semibold cursor-pointer items-center gap-3 rounded-full bg-primary px-4 py-2 text-white">
                           Create New Market
                         </div>
                       ) :
