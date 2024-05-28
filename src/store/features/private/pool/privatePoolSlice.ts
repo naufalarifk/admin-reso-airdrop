c6f71bd87691d25dbfb9ef4a784743d376fa69cb
azharluthfi14 <azharluthfi4@gmail.com>
2024-05-28 16:05:24 +0700

    chore: create module private  pool market

--- src/store/features/private/pool/privatePoolSlice.ts
+++ src/store/features/private/pool/privatePoolSlice.ts
@@ -1,9 +1,31 @@
 import { baseApi } from '@/api/config';
 import type { PrivatePoolState } from '@/store/types';
+import { buildQueryString } from '@/utils';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+type StateMarket =
+   | 'pending'
+   | 'validate_fee'
+   | 'validate_listing'
+   | 'validate_pair'
+   | 'paid'
+   | 'cancel'
+   | 'rejected';
+
+interface PrivatePoolMarketParams {
+   state: StateMarket;
+   currency: string;
+   pair_currency: string;
+   market: string;
+}
+
 const initialState: PrivatePoolState = {
-   getPollCurrencies: {
+   getPoolCurrencies: {
+      isLoading: false,
+      isSuccess: false,
+      data: [],
+   },
+   getPoolMarket: {
       isLoading: false,
       isSuccess: false,
       data: [],
@@ -22,6 +44,18 @@ export const getPrivatePoolCurrencies = createAsyncThunk(
    },
 );
 
+export const getPrivatePoolMarketPairUser = createAsyncThunk(
+   'private/getPrivatePoolMarket',
+   async (params: PrivatePoolMarketParams, { rejectWithValue }) => {
+      try {
+         const response = await baseApi.get(`/trade/pool/market?${buildQueryString(params)}`);
+         return response;
+      } catch (error) {
+         return rejectWithValue(error);
+      }
+   },
+);
+
 const privatePoolSlice = createSlice({
    name: 'privatePoolSlice',
    initialState,
@@ -29,19 +63,32 @@ const privatePoolSlice = createSlice({
    extraReducers: builder => {
       builder
          .addCase(getPrivatePoolCurrencies.pending, state => {
-            state.getPollCurrencies.isLoading = true;
-            state.getPollCurrencies.isSuccess = false;
+            state.getPoolCurrencies.isLoading = true;
+            state.getPoolCurrencies.isSuccess = false;
          })
          .addCase(getPrivatePoolCurrencies.fulfilled, (state, action) => {
-            state.getPollCurrencies.isLoading = false;
-            state.getPollCurrencies.isSuccess = true;
-            state.getPollCurrencies.data = action.payload.data;
+            state.getPoolCurrencies.isLoading = false;
+            state.getPoolCurrencies.isSuccess = true;
+            state.getPoolCurrencies.data = action.payload.data;
          })
          .addCase(getPrivatePoolCurrencies.rejected, (state, action) => {
-            state.getPollCurrencies.isLoading = false;
-            state.getPollCurrencies.isSuccess = true;
-            state.getPollCurrencies.data = [];
-            state.getPollCurrencies.error = action.payload;
+            state.getPoolCurrencies.isLoading = false;
+            state.getPoolCurrencies.isSuccess = true;
+            state.getPoolCurrencies.data = [];
+            state.getPoolCurrencies.error = action.payload;
+         })
+         .addCase(getPrivatePoolMarketPairUser.pending, state => {
+            state.getPoolMarket.isLoading = true;
+            state.getPoolMarket.isSuccess = false;
+         })
+         .addCase(getPrivatePoolMarketPairUser.fulfilled, (state, action) => {
+            state.getPoolMarket.isLoading = false;
+            state.getPoolMarket.isSuccess = true;
+            state.getPoolMarket.data = action.payload.data;
+         })
+         .addCase(getPrivatePoolMarketPairUser.rejected, (state, action) => {
+            state.getPoolMarket.isSuccess = false;
+            state.getPoolMarket.error = action.payload;
          });
    },
 });
