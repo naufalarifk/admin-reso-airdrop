52101c66aeb1f1569ff8a11837fb1e96eb1fc49c
azharluthfi14 <azharluthfi4@gmail.com>
2024-05-28 15:10:43 +0700

    chore: fix module packages

--- src/constants/chains/polygon.ts
+++ src/constants/chains/polygon.ts
@@ -1,25 +1,23 @@
-import { type Chain } from "viem";
-
 export const matic = {
-  name: "Polygon Mainnet",
-  rpcUrls: {
-    default: {
-      http: ["https://polygon-rpc.com/"],
-    },
-  },
-  nativeCurrency: {
-    name: "MATIC",
-    symbol: "MATIC",
-    decimals: 18,
-  },
-  id: 137,
-  blockExplorers: {
-    default: {
-      name: "polygonscan",
-      url: "https://polygonscan.com",
-    },
-  },
-  custom: {
-    icon: "https://miro.medium.com/v2/resize:fit:2400/1*jGa6EsELZlUPWjgPegEmqA.png",
-  },
-} as const satisfies Chain;
+   name: 'Polygon Mainnet',
+   rpcUrls: {
+      default: {
+         http: ['https://polygon-rpc.com/'],
+      },
+   },
+   nativeCurrency: {
+      name: 'MATIC',
+      symbol: 'MATIC',
+      decimals: 18,
+   },
+   id: 137,
+   blockExplorers: {
+      default: {
+         name: 'polygonscan',
+         url: 'https://polygonscan.com',
+      },
+   },
+   custom: {
+      icon: 'https://miro.medium.com/v2/resize:fit:2400/1*jGa6EsELZlUPWjgPegEmqA.png',
+   },
+} as const;
